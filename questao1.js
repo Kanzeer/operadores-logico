d44bb5c660d8ef94383e9920629186e66b2c7eb5
Kanzeer <miguelroaamaral@gmail.com>
2024-03-25 14:54:36 -0400

    questão 1 finalizada

--- questao1.js
+++ questao1.js
@@ -1,5 +1,8 @@
-//O resultado de (10 > 5) && (7 < 9) é verdadeiro ou falso?
+//1. O resultado de (10 > 5) && (7 < 9) é verdadeiro ou falso?
 
 /*(10 > 5) --> true, (7 < 9) --> true
             true && true --> true
 */
+
+let resultado = (10 > 5) && (7 < 9);
+console.log(resultado);
