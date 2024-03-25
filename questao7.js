9e16dead64dfb11d90fcb2bcef5147cc8634fdee
Kanzeer <miguelroaamaral@gmail.com>
2024-03-25 16:00:30 -0400

    questão 7 finalizada

---
+++ questao7.js
@@ -0,0 +1,10 @@
+//7. Qual é o resultado de true && (false || true)?
+
+/*
+    true && (false || true)
+    true && (true)
+        true
+*/
+
+let resultado = (true && (false || true));
+console.log(resultado);
