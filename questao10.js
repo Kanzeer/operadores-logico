511ad487096549cd235bd2c9c3aad754a6f3a345
Kanzeer <miguelroaamaral@gmail.com>
2024-03-25 16:21:57 -0400

    questão 10 finalizada

---
+++ questao10.js
@@ -0,0 +1,10 @@
+//10. Qual é o resultado de !true || false && true?
+
+/*
+    !true || false && true
+    !true || false
+    !true || false = false
+*/
+
+let resultado = (!true || false && true);
+console.log(resultado);
