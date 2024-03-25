62e5bfc8eed2563580de3fc2023d0e1343243ab7
Kanzeer <miguelroaamaral@gmail.com>
2024-03-25 16:42:01 -0400

    questão 11 finalizada

---
+++ questao11.js
@@ -0,0 +1,6 @@
+//11. Explique a diferença entre os operadores && e || em JavaScript.
+
+/*
+    "&&", quando utilizado numa conta, por exemplo true && false, o false sera priorizado, dando o resultado em false
+    "||", quando utilizado numa conta, por exemplo true && false, o true sera priorizado, dando o resultado em true
+*/
