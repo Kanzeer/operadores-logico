4faf63f0f4a673bd68be49115ac61f1aa01de8c3
Kanzeer <miguelroaamaral@gmail.com>
2024-03-25 16:42:18 -0400

    questão 13 finalizada

---
+++ questao13.js
@@ -0,0 +1,10 @@
+//O que significa a expressão !(5 !== 5)?
+
+/*
+    !(5 !== 5)
+    !(false(5 não é diferente de 5))
+    !(false) = true
+*/
+
+let resultado = (!(5 !== 5));
+console.log(resultado);
