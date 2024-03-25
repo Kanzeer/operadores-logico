387c643c4205b84bddbfca250296f1d456efd185
Kanzeer <miguelroaamaral@gmail.com>
2024-03-25 17:24:25 -0400

    questão 16 finalizada

---
+++ questao16.js
@@ -0,0 +1,9 @@
+//16. Qual é o resultado de (true && true) || (false && false)?
+/*
+    (true && true) || (false && false)
+    (true) || (false)
+    (true) || (false) = true
+*/
+
+let resultado = ((true && true) || (false && false));
+console.log(resultado);
