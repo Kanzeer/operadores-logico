25083bf4088cd079323364aa28a755a7c6ef938d
Kanzeer <miguelroaamaral@gmail.com>
2024-03-25 17:24:46 -0400

    questão 17 finalizada

---
+++ questao17.js
@@ -0,0 +1,6 @@
+//17. Explique o que significa a expressão !(10 <= 5). 
+
+/*
+    !(10 <= 5), "10 <= 5", essa expressão diz que 5 é maior ou igual a 10, sendo false
+                "!(false)", resulta em true, pois o "!" serve de oposição
+*/
