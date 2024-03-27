a80f73543633f3cd6b46ef71f475bccc331177bd
Kanzeer <miguelroaamaral@gmail.com>
2024-03-27 14:53:32 -0400

    questão 27 finalizada

---
+++ questao27.js
@@ -0,0 +1,10 @@
+//27. Qual é o resultado de (5 === 5) || !(4 !== 4)?
+
+/* 
+    (5 === 5) || !(4 !== 4)]
+    true || !false
+    true || true = true
+*/
+
+let resultado = (5 === 5) || !(4 !== 4);
+console.log(resultado); //true
