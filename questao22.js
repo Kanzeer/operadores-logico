3aad11b50ca7c84cbaaa442fbd2c62b602b4e5be
Kanzeer <miguelroaamaral@gmail.com>
2024-03-27 14:09:34 -0400

    questão 22 finalizada

---
+++ questao22.js
@@ -0,0 +1,9 @@
+//22. O que retorna a expressão false || (true && false)? 
+
+/*
+    false || (true && false)
+    false ||  false = false
+*/
+
+let resultado = (false || (true && false))
+console.log(resultado); //false
